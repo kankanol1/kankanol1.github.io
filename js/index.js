4f7792bf3761facf5d5425202d023c1d415daf66
lidianzhong <lidianzhong@gl-data.com>
2019-04-15 10:34:10 +0800

    site

--- js/index.js
+++ js/index.js
@@ -5,140 +5,198 @@ const dataPc = [
 
     {
         name: "催菜星空",
-        src: './img/star.png',
+        src: './img/star.jpg',
         href: "./assets/sky/"
     },
     {
         name: "蘑菇街装",
-        src: './img/mogujie.png',
+        src: './img/mogujie.jpg',
         href: "./assets/mogujie/"
     },
     {
         name: "儿童写真",
-        src: './img/marry.png',
+        src: './img/marry.jpg',
         href: "./assets/marry/"
     },
     {
         name: "视频截图",
-        src: './img/mv.png',
+        src: './img/mv.jpg',
         href: "./assets/scripmv/"
     },
     {
         name: "星巴咖啡",
-        src: './img/cof.png',
+        src: './img/cof.jpg',
         href: "./assets/star/"
     },
     {
         name: "京剧高亮",
-        src: './img/jing.png',
+        src: './img/jing.jpg',
         href: "./assets/jingju/"
     },
     {
         name: "陀螺仪图",
-        src: './img/tuo.png',
+        src: './img/tuo.jpg',
         href: "./assets/css/allperspective/"
     },
     {
         name: "炫酷轮播",
-        src: './img/xuan.png',
+        src: './img/xuan.jpg',
         href: "./assets/css/cssstyle/"
     },
     {
         name: "六面炫图",
-        src: './img/six.png',
+        src: './img/six.jpg',
         href: "./assets/css/sixrotate/"
     },
     {
         name: "左右集图",
-        src: './img/lr.png',
+        src: './img/lr.jpg',
         href: "./assets/css/trotatey/"
     },
     {
         name: "上下相册",
-        src: './img/shang.png',
+        src: './img/shang.jpg',
         href: "./assets/css/trotatex/"
     },
     {
         name: "王者战报",
-        src: './img/wang.png',
+        src: './img/wang.jpg',
         href: "./assets/wang/"
     },
     {
         name: "雪夜长安",
-        src: './img/snow.png',
+        src: './img/snow.jpg',
         href: "./assets/css/snow/"
     },
     {
         name: "观澜数据",
-        src: './img/gl.png',
+        src: './img/gl.jpg',
         href: "./assets/glsite/"
     },
     {
         name: "图片切入",
-        src: './img/qie.png',
+        src: './img/qie.jpg',
         href: "./assets/api/"
     },
     {
         name: "瀑布流图",
-        src: './img/pu.png',
+        src: './img/pu.jpg',
         href: "./assets/pu/"
     },
     {
         name: "绘制网络",
-        src: './img/echart.png',
+        src: './img/echart.jpg',
         href: "./assets/Echarts/"
     },
     {
         name: "移动回放",
-        src: './img/move.png',
+        src: './img/move.jpg',
         href: "./assets/move"
     },
 
     {
         name: "方格定位",
-        src: './img/fange.png',
+        src: './img/fange.jpg',
         href: "./assets/fangge"
     },
     {
         name: "图片拖拽",
-        src: './img/dragp.png',
+        src: './img/dragp.jpg',
         href: "./assets/dragp"
     },
     {
         name: "你画我猜",
-        src: './img/hello.png',
+        src: './img/hello.jpg',
         href: "./assets/guess"
     },
     {
         name: "碰撞检测",
-        src: './img/hell.png',
+        src: './img/hell.jpg',
         href: "./assets/peng"
     },
 
 ];
 const dataPhone = [
-
     {
-        name: "方格定位",
-        src: './img/fange.png',
-        href: "./assets/fangge"
+        name: "催菜星空",
+        src: './img/star.jpg',
+        href: "./assets/sky/"
     },
     {
-        name: "CSDN主页",
-        src: './img/csdn.jpg',
-        href: "https://blog.csdn.net/qq_38025939"
+        name: "蘑菇街装",
+        src: './img/mogujie.jpg',
+        href: "./assets/mogujie/"
     },
     {
-        name: "segmentFault主页",
-        src: './img/github.jpg',
-        href: "https://segmentfault.com/u/kankanol1"
+        name: "儿童写真",
+        src: './img/marry.jpg',
+        href: "./assets/marry/"
     },
     {
-        name: "微博主页",
-        src: './img/weibo.jpg',
-        href: "https://weibo.com/3029315223"
-    }
-
+        name: "视频截图",
+        src: './img/mv.jpg',
+        href: "./assets/scripmv/"
+    },
+    {
+        name: "星巴咖啡",
+        src: './img/cof.jpg',
+        href: "./assets/star/"
+    },
+    {
+        name: "陀螺仪图",
+        src: './img/tuo.jpg',
+        href: "./assets/css/allperspective/"
+    },
+    {
+        name: "炫酷轮播",
+        src: './img/xuan.jpg',
+        href: "./assets/css/cssstyle/"
+    },
+    {
+        name: "六面炫图",
+        src: './img/six.jpg',
+        href: "./assets/css/sixrotate/"
+    },
+    {
+        name: "左右集图",
+        src: './img/lr.jpg',
+        href: "./assets/css/trotatey/"
+    },
+    {
+        name: "上下相册",
+        src: './img/shang.jpg',
+        href: "./assets/css/trotatex/"
+    },
+    {
+        name: "王者战报",
+        src: './img/wang.jpg',
+        href: "./assets/wang/"
+    },
+    {
+        name: "雪夜长安",
+        src: './img/snow.jpg',
+        href: "./assets/css/snow/"
+    },
+    {
+        name: "观澜数据",
+        src: './img/gl.jpg',
+        href: "./assets/glsite/"
+    },
+    {
+        name: "图片切入",
+        src: './img/qie.jpg',
+        href: "./assets/api/"
+    },
+    {
+        name: "瀑布流图",
+        src: './img/pu.jpg',
+        href: "./assets/pu/"
+    },
+    {
+        name: "绘制网络",
+        src: './img/echart.jpg',
+        href: "./assets/Echarts/"
+    },
 ];
 
 const dataDraw = [
