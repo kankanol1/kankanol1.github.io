4edc83b0b3259ca6f24c93720a0d6ef1596e3f4a
lidianzhong <lidianzhong@gl-data.com>
2019-05-08 10:01:06 +0800

    冰原狼

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@
 const data = [
     {
         name: "冰原狼",
-        url: "./"
+        url: "https://kankan.fun"
     },
     {
         name: "优质项目推荐",
@@ -30,6 +30,17 @@ const data = [
 
 let navBar = document.querySelector('.nav-bar');
 let navUl = document.createElement('ul');
+let navLiI = document.createElement('li');
+let navAI = document.createElement('a');
+let navImg = document.createElement('img');
+navLiI.appendChild(navAI);
+navAI.appendChild(navImg);
+navAI.href = "https://kankan.fun";
+navAI.classList = 'item';
+navImg.src = './img/favicon.ico';
+navImg.classList = "title";
+navUl.appendChild(navLiI);
+
 for(let i=0;i<data.length-1;i++){
     let navLi = document.createElement('li');
     let navA = document.createElement('a');
@@ -43,8 +54,9 @@ for(let i=0;i<data.length-1;i++){
 let img = document.createElement('img');
 
 img.src = data[data.length-1].url;
+img.classList = "img";
 navBar.appendChild(img);
-navUl.classList = 'clearfix';
+navUl.classList = 'clearfix ul-be';
 navBar.appendChild(navUl);
 
 let flag = true;
@@ -65,6 +77,6 @@ img.addEventListener('click',function(){
 
 });
 document.querySelector('nav').addEventListener('mouseleave',function(){
-    document.querySelector('nav ul').classList = '';
+    // document.querySelector('nav ul').classList = '';
     flag = !flag;
 });
