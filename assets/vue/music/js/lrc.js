8400c593702f8ce0f7ca836dce89dc416b6d1975
lidianzhong <lidianzhong@gl-data.com>
2019-05-14 22:49:42 +0800

    冰原狼

--- assets/vue/music/js/lrc.js
+++ assets/vue/music/js/lrc.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
 
         if( curTime>result[flagN+1][0]){
             for(let i=0;i<result.length;i++){
-                if((curTime >result[flagN][0]) && (curTime<result[flagN+1][0])) flagN = i;
+                if((curTime >result[i][0]) && (curTime<result[i+1][0])) flagN = i;
             }
         }
 
