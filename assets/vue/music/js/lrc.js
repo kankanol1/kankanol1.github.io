9643cee6ed53d3895b381e58f8db5102e6074b7b
lidianzhong <lidianzhong@gl-data.com>
2019-05-15 18:45:26 +0800

    冰原狼

--- assets/vue/music/js/lrc.js
+++ assets/vue/music/js/lrc.js
@@ -2,30 +2,61 @@ let arr = str.split('\n');
 $(document).ready(function () {
     let lrc = []; //创建歌词数组;
     lrc = arr;
-    let myAudio = $("audio")[0];
+    let myAudio = $("#mp")[0];
     let result = [];
     getLrc();
     let flagN = 0;
     function getLrc() {
-        let timeReg = /\[\d{2}:\d{2}\.\d{2}\]/g;//匹配时间的正则表达式
+        let timeReg1 = /\[\d{2}:\d{2}\.\d{2}\]/g;// || /\[\d{2}:\d{2}\.\d{3}\]/g;//匹配时间的正则表达式
+        let timeReg2 = /\[\d{2}:\d{2}\.\d{3}\]/g;// || /\[\d{2}:\d{2}\.\d{3}\]/g;//匹配时间的正则表达式
+        // console.log(timeReg);
+
         for (let i=0;i<lrc.length;i++) {
-            let time = lrc[i].match(timeReg); //获取歌词里的时间
-            let value = lrc[i].replace(timeReg, ""); //获取纯歌词文本
+            let time;
+            let value;
+            if(lrc[i].match(timeReg1)){
+                time = lrc[i].match(timeReg1);
+                value = lrc[i].replace(timeReg1, "")
+            }
+            if(lrc[i].match(timeReg2)){
+                time = lrc[i].match(timeReg2);
+                value = lrc[i].replace(timeReg2, "")
+            }
+            // time = lrc[i].match(timeReg1) || lrc[i].match(timeReg2);
+            // value = lrc[i].replace(timeReg1, "") || lrc[i].replace(timeReg2, "");
+
+
+
+            // let time = lrc[i].match(timeReg2) || lrc[i].match(timeReg1); //获取歌词里的时间
+            // let value = lrc[i].replace(timeReg2, "")  ||  lrc[i].replace(timeReg1, ""); //获取纯歌词文本
+            // console.log(time);
             for (let j=0;j<time.length;j++ ) {
                 let t = time[j].slice(1, -1).split(":"); //t[0]分钟，t[1]秒
+                // console.log(t);
                 let timeArr = parseInt(t[0], 10) * 60 + parseFloat(t[1]);
                 result.push([timeArr, value]);//以[时间(秒)，歌词]的形式存进result
             }
         }
+
+        console.log(result);
         setInterval(showLrc, 100);//设置定时，每200毫秒更新一下
     }
     function showLrc() {
         let curTime = myAudio.currentTime;//获取当前的播放时间
-        if( curTime>result[flagN+1][0] ||  curTime<result[flagN][0] ){
-            for(let i=0;i<result.length;i++){
+        // console.log(myAudio.currentTime);
+
+        if( curTime>result[flagN+1][0] || curTime<result[flagN][0] ){
+            for(let i=1;i<result.length-1;i++){
                 if((curTime >result[i][0]) && (curTime<result[i+1][0])) flagN = i;
+                if((curTime >result[i-1][0]) && (curTime<result[i][0])) flagN = i-1;
             }
         }
+
+        /*if(curTime<result[flagN][0] ){
+            for(let i=0;i<result.length-1;i++){
+                if((curTime >result[i-1][0]) && (curTime<result[i][0])) flagN = i;
+            }
+        }*/
         if ((curTime >result[flagN][0]) && (curTime<result[flagN+1][0]) && document.getElementById("lyric").innerHTML !== result[flagN][1]) {
             document.getElementById("lyric").innerHTML = result[flagN][1];
             flagN = (flagN===result.length-2) ? 0 : flagN+1;
